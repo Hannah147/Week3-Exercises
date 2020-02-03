1f170814d0b95d9ce60f6d56d7347ba99fc99ec6
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-02-03 15:55:27 +0000

    Exercise 4 - Complete

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -11,28 +11,47 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+            WriteLine("Query Syntax");
+            QuerySyntax();
 
-            var query = from number in numbers
-                        select DoubleIt(number);
-
-            var query2 = numbers
-                .Select(n => DoubleIt(n));
-
-            WriteLine("Before the foreach loop");
-
-            foreach(var item in query)
-            {
-                WriteLine($"{item}\n");
-            }
-
-
+            WriteLine("Lambda Syntax");
+            LambdaSyntax();
         }
 
         private static int DoubleIt(int value)
         {
             WriteLine("About to double the number " + value.ToString());
             return value * 2;
+        }
+
+        static void QuerySyntax()
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query = from number in numbers
+                        select DoubleIt(number);
+
+            WriteLine("Before the foreach loop");
+
+            foreach (var item in query)
+            {
+                WriteLine($"{item}\n");
+            }
+        }
+
+        static void LambdaSyntax()
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query2 = numbers
+                .Select(n => DoubleIt(n));
+
+            WriteLine("Before the foreach loop");
+
+            foreach (var item in query2)
+            {
+                WriteLine($"{item}\n");
+            }
         }
     }
 }
