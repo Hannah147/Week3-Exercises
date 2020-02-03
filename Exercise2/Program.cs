0bb485226791a9498bc4b7a5d04713957e9d9bf9
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-02-03 15:35:53 +0000

    Exercise 2 - Complete

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
 
 namespace Exercise2
 {
@@ -10,6 +12,64 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
+            WriteLine("Query Syntax");
+            QuerySyntax();
+
+            WriteLine("Lambda Syntax");
+            LambdaSyntax();
+
         }
+        static void QuerySyntax()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+
+            WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach(var item in query)
+            {
+                WriteLine("{0} \t{1} bytes, \t{2}", item.Name, item.Length, item.CreationTime);
+            }
+
+            WriteLine("\n\n\n");
+        }
+
+        static void LambdaSyntax()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = files
+                .Where(f => f.Length > 10000)
+                .OrderBy(f => f.Length).ThenBy(f => f.Name)
+                .Select(f => new MyFileInfo
+                {
+                    Name = f.Name,
+                    Length = f.Length,
+                    CreationTime = f.CreationTime
+                });
+
+            WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach (var item in query)
+            {
+                WriteLine("{0} \t{1} bytes, \t{2}", item.Name, item.Length, item.CreationTime);
+            }
+        }
+    }
+
+    public class MyFileInfo
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 }
