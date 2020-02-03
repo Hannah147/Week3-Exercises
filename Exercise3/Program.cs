377f93fe3e0bc00af9319a531f458f063368da59
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-02-03 15:37:43 +0000

    Created Exercise 3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Query Syntax");
+            QuerySyntax();
+
+            WriteLine("Lambda Syntax");
+            LambdaSyntax();
+        }
+
+        static void QuerySyntax()
+        {
+
+        }
+
+        static void LambdaSyntax()
+        {
+
+        }
+    }
+}
