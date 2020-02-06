975d12e62562222e3f6f19768d97743624d09f80
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-02-06 11:35:36 +0000

    Created Exercise 6

---
+++ Exercise6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Query Syntax");
+            QuerySyntax();
+
+            WriteLine("\n\nLambda Syntax");
+            LambdaSyntax();
+        }
+
+        static void QuerySyntax()
+        {
+
+        }
+
+        static void LambdaSyntax()
+        {
+
+        }
+    }
+}
