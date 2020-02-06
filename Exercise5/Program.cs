b1f52f0d40ae9c1ac853508cc73e70b629c58eac
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-02-06 11:31:21 +0000

    Created and completed Exercise 5

---
+++ Exercise5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Query Syntax");
+            QuerySyntax();
+
+            WriteLine("\n\nLambda Syntax");
+            LambdaSyntax();
+        }
+
+        private static int DoubleIt(int value)
+        {
+            WriteLine("About to double the number " + value.ToString());
+            return value * 2;
+        }
+
+        static void QuerySyntax()
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query1 = from number in numbers
+                         orderby number descending
+                         select number;
+
+            var query2 = from number in query1
+                         where number < 8
+                         select number;
+
+            var query3 = from number in query2
+                         select DoubleIt(number);
+
+            foreach (var item in query3)
+            {
+                WriteLine(item);
+            }
+        }
+
+        static void LambdaSyntax()
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query1 = numbers.OrderByDescending(n => n);
+
+            var query2 = query1.Where(n => n < 8);
+
+            var query3 = query2.Select(n => DoubleIt(n));
+
+            foreach(var item in query3)
+            {
+                WriteLine(item);
+            }
+
+            var query4 = numbers
+                .OrderByDescending(n => n)
+                .Where(n => n < 8)
+                .Select(n => DoubleIt(n));
+        }
+    }
+}
